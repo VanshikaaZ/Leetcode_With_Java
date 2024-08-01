434dfc669fc568d4dea666b97702f2b832dbd46d
Vanshika Grover <169062727+VanshikaaZ@users.noreply.github.com>
2024-08-01 13:11:59 +0530

    Time: 0 ms (100%), Space: 42.4 MB (68.62%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -1,7 +1,7 @@
 class Solution {
     public int countSeniors(String[] details) {
         int ans = 0;
-        for(int i=0; i<details.length; i++){
+        for(int i=0; i< details.length; i++){
             int age = (details[i].charAt(11) - '0') * 10 + (details[i].charAt(12) - '0');
             if(age > 60) ans++;
         }
